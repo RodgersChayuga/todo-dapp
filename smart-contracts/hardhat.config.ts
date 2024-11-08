42a6fbcce228f0e62d87655863404c00604436a0
Rodgers Chayuga <chayugarodgers@gmail.com>
2024-11-09 00:09:05 +0300

    Smart-Contracts | Add Hardhat configuration

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,32 @@
+// contracts/hardhat.config.ts
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
+  paths: {
+    artifacts: "../src/artifacts",
+  },
 };
 
 export default config;
